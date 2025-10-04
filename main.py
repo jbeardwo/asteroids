d9acc08d0c5f5a0a41b36d8647a92d0f63dc8ae9
jbeardwo <jfwbeardwood@gmail.com>
2025-10-03 17:54:35 -0700

    switched update and draw loops to groups

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame
 from constants import *
-from player import Player
 from circleshape import CircleShape
+from player import Player
 
 def main():
     import os
     os.environ['SDL_VIDEODRIVER'] = 'x11'
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -17,8 +21,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
