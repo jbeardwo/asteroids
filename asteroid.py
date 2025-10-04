603640f87776f406624c792c9f6e3cb34e37f12b
jbeardwo <jfwbeardwood@gmail.com>
2025-10-04 11:32:43 -0700

    added shot functionality and asteroid splitting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 from constants import *
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,4 +13,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        new_vel_1 = self.velocity.rotate(angle)
+        new_vel_2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = new_vel_1 * 1.2
+        new_asteroid_2.velocity = new_vel_2 * 1.2
 
--- main.py
+++ main.py
@@ -31,16 +31,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0,0,0))
+
         for object in updatable:
             object.update(dt)
         for asteroid in asteroids:
             if asteroid.collide(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if asteroid.collide(shot):
+                    shot.kill()
+                    asteroid.split()
         for object in drawable:
             object.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
